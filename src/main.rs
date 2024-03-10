8ea89e94bdc5873657e667fa23aa3740c81b496f
Calm0406 <calm0406@163.com>
2024-03-10 12:22:47 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 #[tokio::main]
 async fn main() {
     println!("Hello, world!2123123");
-    test().await
+    test().await;
+    test1().await
 }
 async fn test(){
+    println!("123123123")
+}
+async fn test1(){
     println!("123123123")
 }
