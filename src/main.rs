e54096f7fc8c681528fb5b211e5732e339d095c6
Calm0406 <calm0406@163.com>
2024-03-10 12:32:00 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ async fn main() {
     println!("Hello, world!2123123");
     test0().await;
     test1().await;
-    test2().await
+    test2().await;
 }
 async fn test0(){
     println!("123123123")
